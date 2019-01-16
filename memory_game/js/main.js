4fd7b91064b1b68e54476ea1b4006a3e0638271b
Isobel Barnes <IsobelBarnes@isobels-mbp.home>
2019-01-16 20:36:09 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,19 @@
+
+
 console.log("up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-console.log ("User flipped " + cardOne);
+var cardOne = cards[0];
+var cardTwo = cards[2];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+console.log("User flipped" + " " + cardOne);
+console.log("User flipped" + " " + cardTwo);
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+}
